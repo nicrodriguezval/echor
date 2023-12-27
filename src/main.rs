ff1c03cf1b215e60ed1709dae4686014896f64c0
nicrodriguezval <nicrodriguezval@gmail.com>
2023-12-26 21:23:01 -0500

    Base code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 use clap::{App, Arg};
 
 fn main() {
+    let text_arg_name = "text";
+    let omit_newline_arg_name = "omit_newline";
+
     let matches = App::new("Echor")
         .version("0.1.0")
         .author("Nicolás Rodríguez <nicrodriguezval@gmail.com>")
         .about("Rust echo")
         .arg(
-            Arg::with_name("text")
+            Arg::with_name(text_arg_name)
                 .value_name("TEXT")
                 .help("Input text")
                 .required(true)
                 .min_values(1)
         )
         .arg(
-            Arg::with_name("omit_newline")
+            Arg::with_name(omit_newline_arg_name)
                 .short("n")
                 .help("Omit trailing newline")
                 .takes_value(false)
         )
         .get_matches();
 
-    println!("{:#?}", matches);
+    let text = matches.values_of_lossy(text_arg_name).unwrap().join(" ");
+    let ending = if matches.is_present(omit_newline_arg_name) { "" } else { "\n" };
+
+    print!("{}{}", text, ending);
 }
