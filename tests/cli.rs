7b3ba24369c0341eeab1c8444bf78c57fdd58dc5
nicrodriguezval <nicrodriguezval@gmail.com>
2023-12-31 21:07:45 -0500

    hello1.txt test

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
 
 const BIN: &'static str = "echor";
 
@@ -18,4 +19,16 @@ fn runs() {
         .arg("-n")
         .arg("hello")
         .assert().success();
+}
+
+#[test]
+fn hello_1() {
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    cmd
+        .arg("Hello there")
+        .assert()
+        .success()
+        .stdout(expected);
 }
